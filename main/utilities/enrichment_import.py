d316a753569c0e6c2bea1d088ac16cb653f130f5
Mark Vismonte <mark.vismonte@gmail.com>
2012-05-10 02:06:08 -0700

    Made changes to the admin page
    - Removed the Animal observations model from being shown in admin
    - Added the AnimalObservation inline to the Observation and Animal Admins
    - Added the folder main/utilities to create the enrichment_import.py script
    - This script will be used to import data into the database for testing

---
+++ main/utilities/enrichment_import.py
@@ -0,0 +1,5 @@
+# Copyright 2012 PAWS. All rights reserved.
+# Date: 5/5/2012
+# Main Utility Enrichment importer
+# This is utility script used to import species and enrichments into the
+# database.
